f829f145c94b29293a6596d57c443bb661d05168
borsuczyna <libxxerty@gmail.com>
2023-07-07 19:05:51 +0200

    s

--- js/block.js
+++ js/block.js
@@ -1,30 +1,35 @@
 let currentTab = 0; // Current tab is set to be the first tab (0)
+let targetTab = 0;
+let values = {};
 let tabs = [
     "inicio", "tema", "publico", "aulas", "extras", "material"
 ];
 
 function setTab(id) {
-    let child = document.querySelectorAll('.tabs .item')[id];
-    if(!child) return;
-
-    document.querySelectorAll('.tabs .item').forEach((item) => item.classList.remove('active'));
-    child.classList.add('active');
-
-    currentTab = id;
-    updateEmbed();
-    updateArrows();
+    let embed = document.querySelector('.block #content');
+    embed.contentWindow.postMessage('getValue', '*');
+    targetTab = id;
 }
 
 function nextTab() {
-    if(currentTab >= tabs.length - 1) currentTab = 0;
-    else currentTab++;
-    setTab(currentTab);
+    if(currentTab == tabs.length - 1) {
+        alert(`
+        ${JSON.stringify(values).replace(/,/g, ',\n')}
+        `)
+        return;
+    }
+
+    let id = currentTab;
+    if(id >= tabs.length - 1) id = 0;
+    else id++;
+    setTab(id);
 }
 
 function prevTab() {
-    if(currentTab <= 0) currentTab = tabs.length - 1;
-    else currentTab--;
-    setTab(currentTab);
+    let id = currentTab;
+    if(id <= 0) id = tabs.length - 1;
+    else id--;
+    setTab(id);
 }
 
 function updateEmbed() {
@@ -33,10 +38,30 @@ function updateEmbed() {
 }
 
 window.addEventListener("message", receiveMessage, false);
-  
+
 function receiveMessage(event) {
     if(event.data == 'nextTab') nextTab();
     else if(event.data == 'prevTab') prevTab();
+    else if(event.data == 'getValue') {
+        let embed = document.querySelector('.block #content');
+        let value = values[currentTab];
+        value = value ? (Array.isArray(value) ? value.join('|') : value) : '';
+        embed.contentWindow.postMessage('setValue|' + value);
+    } else if(event.data.startsWith('saveValue')) {
+        let value = event.data.slice(10);
+        value = value.split('|');
+        values[currentTab] = value;
+
+        let child = document.querySelectorAll('.tabs .item')[targetTab];
+        if(!child) return;
+
+        document.querySelectorAll('.tabs .item').forEach((item) => item.classList.remove('active'));
+        child.classList.add('active');
+
+        currentTab = targetTab;
+        updateEmbed();
+        updateArrows();
+    }
 }
 
 function updateArrows() {
