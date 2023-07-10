53308404d48cd6b21a4798ef7be403b1041be7f1
borsuczyna <libxxerty@gmail.com>
2023-07-10 18:17:38 +0200

    Form update

--- js/form.js
+++ js/form.js
@@ -4,3 +4,21 @@ function scrollToForm(id) {
         behavior: 'smooth'
     });
 }
+
+function updateActiveForm() {
+    let formTabs = document.querySelector('.tabs').children;
+    let forms = document.getElementById('forms').children;
+
+    for (let i = 0; i < formTabs.length; i++) {
+        let form = forms[i];
+        if(!form) continue;
+        let input = form.querySelector('input') || form.querySelector('textarea');
+        let focused = document.activeElement === input;
+
+        if (focused) {
+            formTabs[i].classList.add('active');
+        } else {
+            formTabs[i].classList.remove('active');
+        }
+    }
+}
